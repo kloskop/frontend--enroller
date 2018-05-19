88abbde85a43bf1093c76c67101c7e20c9ad6781
student5 <student5@s10n-mwo-25.pcoip.ki.agh.edu.pl>
2018-05-19 11:49:28 +0200

    Added login & logout

--- web2-vue/src/App.vue
+++ web2-vue/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    
-    <h1>Twój e-mail to: {{ email }}</h1>
-    
+    <h1>Witaj w systemie!</h1>
+    <div v-if="!isAuthenticated"> 
+      Zaloguj się e-mailem:
     <input type="email" v-model="email">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <button @click="login()">Zaloguj</button>
+    </div>
+    <div v-else>
+    <h2>Witaj {{email}}!</h2>
+      <button @click="logout()">Wyloguj</button>
+    </div>
+    
+  
+  
+  
+   
+    
+    
   
     
   </div>
@@ -17,29 +26,41 @@
 
 <script>
 export default {
-  
-  name: 'app',
-  data () {
-    return {
-      email: ''
-     }
-  }
-}
+  name: "app",
 
+  data() {
+    return {
+      email: "",
+      isAuthenticated: false
+    };
+  },
+  methods: {
+    login() {
+      this.isAuthenticated = true;
+    },
+    logout() {
+      this.isAuthenticated = false;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 
-h1, h2 {
+h1 {
+  font-weight: bold;
+}
+h2 {
   font-weight: normal;
+  font-size: large;
 }
 
 ul {
